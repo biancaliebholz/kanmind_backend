80f0963b9846c5b73d1625637ac427b8b1a969fa
Bianca Liebholz <liebholz@icloud.com>
2026-08-19 12:51:43 +0200

    feat: add registration endpoint with token authentication

---
+++ auth_app/api/views.py
@@ -0,0 +1,29 @@
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+
+from auth_app.api.serializers import RegistrationSerializer
+from auth_app.models import User
+
+
+class RegistrationView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegistrationSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        token, created = Token.objects.get_or_create(user=user)
+
+        return Response(
+            {
+                "token": token.key,
+                "fullname": user.fullname,
+                "email": user.email,
+                "user_id": user.id,
+            },
+            status=status.HTTP_201_CREATED,
+        )
--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     "rest_framework",
+    "rest_framework.authtoken",
 
     "auth_app",
     "boards_app",
