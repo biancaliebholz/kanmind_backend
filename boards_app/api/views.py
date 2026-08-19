32748eadfbb465c7a26cb0a3f963273f6397162d
Bianca Liebholz <liebholz@icloud.com>
2026-08-19 15:21:34 +0200

    fix: return forbidden for unauthorized board access

--- boards_app/api/views.py
+++ boards_app/api/views.py
@@ -23,9 +23,12 @@ class BoardViewSet(ModelViewSet):
     def get_queryset(self):
         user = self.request.user
 
-        return Board.objects.filter(
-            Q(owner=user) | Q(members=user)
-        ).distinct()
+        if self.action == "list":
+            return Board.objects.filter(
+                Q(owner=user) | Q(members=user)
+            ).distinct()
+
+        return Board.objects.all()
 
     def get_serializer_class(self):
         if self.action == "list":
